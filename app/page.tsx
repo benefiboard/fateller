9505a5a25eed66bd664901095978a419ebaa877d
benefiboard@gmail.com <hjdh59@gmail.com>
2025-02-02 14:06:09 +0900

    0202

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Home, Bell, User, Camera } from 'lucide-react';
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h2>FaTeller1</h2>
+    </div>
+  );
+};
+
+export default HomePage;
